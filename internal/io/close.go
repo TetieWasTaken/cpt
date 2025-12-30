2c6de96206502fb3dfaebb63389484b84ed4d61e
TetieWasTaken <74485845+TetieWasTaken@users.noreply.github.com>
2025-12-30 20:35:10 +0100

    feat(clio): add CloseWithDefer

--- internal/io/close.go
+++ internal/io/close.go
@@ -3,6 +3,8 @@ package clio
 import (
 	"fmt"
 	"io"
+
+	"github.com/spf13/cobra"
 )
 
 func Close(closer io.Closer, onErr func(error)) {
@@ -12,5 +14,11 @@ func Close(closer io.Closer, onErr func(error)) {
 
 	if err := closer.Close(); err != nil && onErr != nil {
 		onErr(fmt.Errorf("error while trying to close: %w", err))
+	}
+}
+
+func CloseWithDefer(cmd *cobra.Command, closer io.Closer) func() {
+	return func() {
+		Close(closer, func(err error) { fmt.Fprintln(cmd.ErrOrStderr(), err) })
 	}
 }
