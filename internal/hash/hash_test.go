08599f24824229d17afb0bb133d9340abf06b94a
TetieWasTaken <74485845+TetieWasTaken@users.noreply.github.com>
2025-12-30 21:07:10 +0100

    feat(test): test all algorithms

--- internal/hash/hash_test.go
+++ internal/hash/hash_test.go
@@ -5,6 +5,9 @@ import (
 	"testing"
 )
 
+// knownHashes are the hashes of the string "The quick brown fox jumps over the lazy dog"
+var knownHashes = map[string]string{"sha256": "d7a8fbb307d7809469ca9abcb0082e4f8d5651e46d3cdb762d02d0bf37c9e592", "md5": "9e107d9d372bb6826bd81d3542a419d6"}
+
 func TestGetAlgorithm(t *testing.T) {
 	_, ok := GetAlgorithm("sha256")
 
@@ -14,21 +17,23 @@ func TestGetAlgorithm(t *testing.T) {
 }
 
 func TestHash(t *testing.T) {
-	hasher, ok := GetAlgorithm("sha256")
+	for algorithm, expectedHash := range knownHashes {
+		hasher, ok := GetAlgorithm(algorithm)
 
-	if !ok {
-		t.Errorf("GetAlgorithm() was unable to find sha256")
-	}
+		if !ok {
+			t.Errorf("GetAlgorithm() was unable to find %s", algorithm)
+		}
 
-	result, error := hasher.Hash(strings.NewReader("The quick brown fox jumps over the lazy dog"))
+		result, error := hasher.Hash(strings.NewReader("The quick brown fox jumps over the lazy dog"))
 
-	if error != nil {
-		t.Errorf("Hash() resulted in an error")
-	}
+		if error != nil {
+			t.Errorf("Hash() resulted in an error")
+		}
 
-	hex := HexDigest(result)
+		hex := HexDigest(result)
 
-	if hex != "d7a8fbb307d7809469ca9abcb0082e4f8d5651e46d3cdb762d02d0bf37c9e592" {
-		t.Errorf("Hash result did not match known hash")
+		if hex != expectedHash {
+			t.Errorf("Hash result did not match known hash")
+		}
 	}
 }
