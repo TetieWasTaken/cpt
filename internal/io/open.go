8fc7a16cb1a73562d36727f3e74f3f53a7e95074
TetieWasTaken <74485845+TetieWasTaken@users.noreply.github.com>
2025-12-30 13:50:53 +0100

    feat(io): add open.go

---
+++ internal/io/open.go
@@ -0,0 +1,30 @@
+package io
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+// ParseInput processes user input from the CLI and returns a stream.
+func ParseInput(args []string, file string) (io.ReadCloser, error) {
+	if file != "" {
+		// Open the specified file to read.
+		return os.Open(file)
+	}
+
+	if len(args) > 0 {
+		// Combine args into a single string and return as reader.
+		return io.NopCloser(strings.NewReader(strings.Join(args, " "))), nil
+	}
+
+	stat, err := os.Stdin.Stat()
+	// Check if stdin is not a terminal (e.g. via piping).
+	if err == nil && stat.Mode()&os.ModeCharDevice == 0 {
+		return io.NopCloser(bufio.NewReader(os.Stdin)), nil
+	}
+
+	return nil, fmt.Errorf("no input provided")
+}
